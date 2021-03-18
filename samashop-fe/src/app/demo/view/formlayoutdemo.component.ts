eb264d61e8efa7334085155e0080311050f0ffd9
bamboguirassy <didegassama@gmail.com>
2021-03-18 17:58:05 +0000

    renaming directories for samashop

---
+++ samashop-fe/src/app/demo/view/formlayoutdemo.component.ts
@@ -0,0 +1,25 @@
+import {Component} from '@angular/core';
+import {BreadcrumbService} from '../../app.breadcrumb.service';
+
+@Component({
+    templateUrl: './formlayoutdemo.component.html'
+})
+export class FormLayoutDemoComponent {
+
+    selectedState: any = null;
+
+    states: any[] = [
+        {name: 'Arizona', code: 'Arizona'},
+        {name: 'California', value: 'California'},
+        {name: 'Florida', code: 'Florida'},
+        {name: 'Ohio', code: 'Ohio'},
+        {name: 'Washington', code: 'Washington'}
+    ];
+
+    constructor(private breadcrumbService: BreadcrumbService) {
+        this.breadcrumbService.setItems([
+            {label: 'UI Kit'},
+            {label: 'Form Layout'}
+        ]);
+    }
+}
---
+++ samashop-fe/src/app/utilities/display.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+import {BreadcrumbService} from '../app.breadcrumb.service';
+
+@Component({
+    templateUrl: './display.component.html'
+})
+export class DisplayComponent {
+
+    constructor(private breadcrumbService: BreadcrumbService) {
+        this.breadcrumbService.setItems([
+            {label: 'Utilities'},
+            {label: 'Display'}
+        ]);
+    }
+}
